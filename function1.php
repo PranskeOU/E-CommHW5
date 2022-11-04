54636b855d98ac4bc4bb672a084f98f35498e08f
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-04 11:36:09 -0500

    Update function1.php

--- function1.php
+++ function1.php
@@ -12,7 +12,7 @@
   function fruits(){
     let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
    
-    return "Here is a fruit:<br>${fruits[a]}."
+    return "Here is a fruit:<br>" + fruits[a]
   }
 </script>
 <button type="button" onclick="myFunction()">List Those Fruits!</button>
