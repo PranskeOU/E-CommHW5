a0fefbc8aac801036211bdaf66b3454eb7cb9897
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-03 22:48:06 -0500

    Update function1.php

--- function1.php
+++ function1.php
@@ -2,6 +2,8 @@
 <p id="fruits"></p>
 <script>
   function fruits(){
+    let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
+    
     return "These are some fruits:<br>Apple<br>Orange<br>Banana"
   }
   document.getElementById("fruits").innerHTML = fruits();
