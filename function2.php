cb8cfd08db23caf7e8433543b57f2e931683a2c3
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-04 12:33:49 -0500

    Update function2.php

--- function2.php
+++ function2.php
@@ -1,5 +1,4 @@
 <?php require_once("header.php"); ?>
-<p id="loadMessage"></p>
 <body onload='myFunction()'>
   <script>
     function myFunction(){
@@ -9,4 +8,5 @@
     function loadMessage(){
       let loadMessage = "There's the second function! See, I bet you didn't even catch it... it was called automatically when the page loaded in!";
       return loadMessage;
+  </script>
 </body>
