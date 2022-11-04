69708bdbc565f3ef82494ef11b1dedd8e8aa6b56
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-04 12:42:27 -0500

    Update function2.php

--- function2.php
+++ function2.php
@@ -1,8 +1,8 @@
 <?php require_once("header.php"); ?>
-  <script type="text/javascript">
+  <script>
     function loadMessage(){
-      let loadMessage = "There's the second function! See, I bet you didn't even catch it... it was called automatically when the page loaded in!";
-      document.write(loadMessage);
+      let loadMessage = "This is it! This is the second function. Click OK and move on!";
+      alert(loadMessage);
     }
     window.onload = loadMessage;
   </script>
