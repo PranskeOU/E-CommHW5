d069373355d15da78cbc5dd9745829ce783a9ebf
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-04 11:47:47 -0500

    Update function1.php

--- function1.php
+++ function1.php
@@ -12,7 +12,7 @@
   }
   function fruits(){
     let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
-    return "Here is a fruit:<br>" + </script><b><script>fruits[a]</script></b><script>;
+    return "Here is a fruit:<br>" + fruits[a];
   }
 </script>
 
