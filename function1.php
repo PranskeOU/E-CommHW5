90f9a76b3d70657cb6ce0c50d7fab92d3a19f609
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-03 23:04:11 -0500

    Update function1.php

--- function1.php
+++ function1.php
@@ -1,11 +1,15 @@
 <?php require_once("header.php"); ?>
 <p id="fruits"></p>
 <script>
+  function myFunction() {
+    document.getElementById("fruits").innerHTML = fruits();
+  }
   function fruits(){
     let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
     
     return "These are some fruits:<br>Apple<br>Orange<br>Banana"
   }
-  document.getElementById("fruits").innerHTML = fruits();
+  <button type="button" onclick="myFunction()">List Those Fruits!</button>
+
 </script>
     
