2f7a01fc41f65ac72cfadcec4972f60d123aa736
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-03 22:32:33 -0500

    Update function1.php

--- function1.php
+++ function1.php
@@ -1,1 +1,9 @@
 <?php require_once("header.php"); ?>
+<p id="fruits"></p>
+<script>
+  function fruits(){
+    return "These are some fruits:\nApple\nOrange\nBanana\n"
+  }
+  document.getElementById("fruits").innerHTML = fruits();
+</script>
+    
