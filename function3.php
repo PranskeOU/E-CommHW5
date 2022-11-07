016d35d92a5dc610b6088796881244546aff81cd
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-07 13:34:37 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -1,1 +1,52 @@
 <?php require_once("header.php"); ?>
+<div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+</div>
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;  
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%; 
+  background: #04AA6D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #04AA6D;
+  cursor: pointer;
+}
+<!--
+<button type="button" onclick="myFunction()">Show me a fruit</button>
+<p id="fruits"></p>
+<script>
+  let a = 0;
+  function myFunction() {
+    document.getElementById("fruits").innerHTML = fruits();
+    a++;
+    if (a==10){
+      a=0;
+    }
+  }
+  function fruits(){
+    let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
+    return "Here is a fruit:<br>" + fruits[a];
+  }
+</script>
+-->
