549752d7e3c0c936b3846cc673e24c4c29d9954e
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-07 13:41:21 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -1,15 +1,6 @@
 <?php require_once("header.php"); ?>
 <div class="slidecontainer">
-  <input type="range" min="1" max="100" value="50" class="slider" id="myRange" 
-  -webkit-appearance="none";
-  width="100%";
-  height="15px";
-  border-radius="5px";  
-  background="#d3d3d3";
-  outline="none";
-  opacity="0.7";
-  -webkit-transition=".2s";
-  transition: opacit=".2s";>
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange" length="200">
 </div>
 
 <!--
