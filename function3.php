bf3a45f26e2428bc6891d5b3bf5ee53d9fb533e3
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-07 14:12:13 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -10,7 +10,7 @@ function drawRectangles() {
 
     const ctx = canvas.getContext('2d');
     var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-    ctx.fillStyle = '#F9DC5C';
+    ctx.fillStyle = randomColor;
     ctx.fillRect(100, 100, 150, 100);
 }
 
