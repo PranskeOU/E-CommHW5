1dd9c21a95d1669779489d35216c1ecd3bfd8770
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-07 18:00:56 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -12,10 +12,6 @@ function drawRectangles() {
     var time = new Date();
     while(time != 'December 31, 2025 00:00:00'){
         var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-        let a=0;
-        for(var i=0;i<100;i++){
-            a++;
-        }
     }
     ctx.fillStyle = randomColor;
     ctx.fillRect(100, 100, 150, 100);
