736d59fda0c7f77ff7aa79d3a84d89b6b4de1490
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-09 10:07:43 -0600

    Update function3.php

--- function3.php
+++ function3.php
@@ -9,7 +9,6 @@ setInterval(function drawRectangles() {
     }
 
     const ctx = canvas.getContext('2d');
-    var time = new Date();
     var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
     ctx.fillStyle = randomColor;
     ctx.fillRect(100, 100, 150, 100);
@@ -17,21 +16,3 @@ setInterval(function drawRectangles() {
 
 drawRectangles();
 </script>
-<!--
-<button type="button" onclick="myFunction()">Show me a fruit</button>
-<p id="fruits"></p>
-<script>
-  let a = 0;
-  function myFunction() {
-    document.getElementById("fruits").innerHTML = fruits();
-    a++;
-    if (a==10){
-      a=0;
-    }
-  }
-  function fruits(){
-    let fruits = [ "Apple", "Orange", "Peach", "Banana", "Plum", "Grape", "Strawberry", "Blueberry", "Mango", "Your mom" ];
-    return "Here is a fruit:<br>" + fruits[a];
-  }
-</script>
--->
