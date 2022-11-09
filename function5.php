bd3da1ae7713c499554f070f12127fab5563f981
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-09 13:16:44 -0600

    Update function5.php

--- function5.php
+++ function5.php
@@ -1,1 +1,13 @@
 <?php require_once("header.php"); ?>
+<p id="counter"></p>
+<script>
+  setInterval(function myFunction(){
+          document.getElementById("counter").innerHTML = counter();
+  },100)
+  function counter(){
+          let i=0;
+          i++;
+          return i;
+  }
+  window.onload = counter();
+</script>
