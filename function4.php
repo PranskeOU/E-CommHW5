06b907698aeece95bd92c7eaa64c4f9b4a668566
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-09 09:40:24 -0600

    Update function4.php

--- function4.php
+++ function4.php
@@ -8,7 +8,7 @@
 </head>
 
 <button type="button" id="Btn" onclick="myFunction()">Show the MIS Logo!</button>
-<img id="Img" src="https://www.ou.edu/price/mis/_jcr_content/headeripar/image.img.png/1534452278211.png" />
+<img class="Img" id="Img" src="https://www.ou.edu/price/mis/_jcr_content/headeripar/image.img.png/1534452278211.png" />
 <script>
 function myFunction(){
 	document.getElementById('Img').src='https://www.ou.edu/price/mis/_jcr_content/headeripar/image.img.png/1534452278211.png'.style.display="block";
